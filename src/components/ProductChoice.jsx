16e9f4527990fefbfa762ed185996aa7c9e26151
eliguus <naoltd164@gmail.com>
2024-07-08 23:50:15 +0300

    product page responsiveness added

--- src/components/ProductChoice.jsx
+++ src/components/ProductChoice.jsx
@@ -1,29 +1,32 @@
 import RatingStars from "./RatingStars"
-const ProductChoice = ({product,name,price,rating}) =>{
+const ProductChoice = ({product,name,price,rating,img}) =>{
     return (
         <>
-        <div className="flex flex-col my-8">
-                    <div className="flex w-[20%] shadow-[#A3A30F77] shadow-md ">
-                        <button className="text-xl bg-[#A3A30F77] p-4">{product}</button>
-                    </div>
-                </div>
-                <div className="flex">
-                    <div className="bg-[#A3A30F77] m-4">
-                        <img src="" alt=""/>
-                        <p>{name}</p>
-                        <RatingStars rating={rating}/>
-                        <p>{price}</p>
-                        <button>add to cart</button>
-                    </div>
-                    <div className="bg-[#A3A30F77] m-4">
-                        <img src="" alt=""/>
-                        <p>{name}</p>
-                        <RatingStars rating={rating}/>
-                        <p>{price}</p>
-                        <button>add to cart</button>
-                    </div>
-                </div>
-        </>
+    <div className="flex">
+        <div className="flex flex-col justify-between bg-[#A3A30F77] m-4 h-full">
+            <div>
+                <img src={img} alt=""/>
+                <p>{name}</p>
+                <RatingStars rating={rating}/>
+                <p>{price}</p>
+            </div>
+            <div>
+                <button className="bg-black text-white ml-auto">Add to cart</button>
+            </div>
+        </div>
+        <div className="flex flex-col justify-between bg-[#A3A30F77] m-4 h-full">
+            <div>
+                <img src={img} alt=""/>
+                <p>{name}</p>
+                <RatingStars rating={rating}/>
+                <p>{price}</p>
+            </div>
+            <div>
+                <button className="bg-black text-white ml-auto">Add to cart</button>
+            </div>
+        </div>
+    </div>
+</>
     )
 }
 
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,31 +1,31 @@
 import ProductChoice from "../components/ProductChoice"
-import RatingStars from "../components/RatingStars"
+import DENIMS from "../assets/HomePage/DENIMS.png"
 
 const ProductsPage = ()=>{
     return (
         <>
         <div className="flex flex-col items-center my-8">
-        <div className="flex w-[80%]">
-        <div className="flex">
-            <div className="W-1/3 flex flex-col">
-                <p>Categories</p>
-                <p>DENIMS</p>
-                <p>GOWNS</p>
-                <p>JEWELRIES</p>
-                <p>SNEAKERS</p>
-                <p>CARGO PANTS</p>
-                <p>T-SHIRTS</p>
-            </div>
-            <div className="W-2/3 flex flex-col">
-                <p>Products</p>
-                <div>
-                    <ProductChoice product={'DENIMS'} name={'NAME'}  price={'3456'} rating={'5'}/>
-                    <ProductChoice product={'DENIMS'} name={'NAME'}  price={'3456'} rating={'5'}/>
-                    <ProductChoice product={'DENIMS'} name={'NAME'}  price={'3456'} rating={'5'}/>
+            <div className="flex w-[80%]">
+                <div className="flex flex-col md:flex-row">
+                    <div className="w-full md:w-1/3 flex flex-col items-center p-4 border-black border rounded-lg m-2 order-last md:order-none">
+                        <p className="text-2xl mb-6">Categories</p>
+                        <p className="text-xl mb-4">DENIMS</p>
+                        <p className="text-xl mb-4">GOWNS</p>
+                        <p className="text-xl mb-4">JEWELRIES</p>
+                        <p className="text-xl mb-4">SNEAKERS</p>
+                        <p className="text-xl mb-4">CARGO PANTS</p>
+                        <p className="text-xl mb-4">T-SHIRTS</p>
+                    </div>
+                    <div className="w-full md:w-2/3 flex flex-col order-first md:order-none">
+                        <p className="text-2xl">Products</p>
+                        <div>
+                            <ProductChoice product={'DENIMS'} name={'NAME'} price={'3456'} rating={'5'} img={DENIMS}/>
+                            <ProductChoice product={'DENIMS'} name={'NAME'} price={'3456'} rating={'5'} img={DENIMS}/>
+                            <ProductChoice product={'DENIMS'} name={'NAME'} price={'3456'} rating={'5'} img={DENIMS}/>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        </div>
         </div>
         </>
     )
