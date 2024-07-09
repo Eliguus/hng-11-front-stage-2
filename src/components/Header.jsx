0b27bc04f97d390fd7c748bac2362568f7d9aadc
eliguus <naoltd164@gmail.com>
2024-07-09 20:18:59 +0300

    some basic changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <>
             <div className='flex-row items-center justify-between hidden md:flex'>
                 <img src={wendyLogo} alt="Wendy Logo" className='w-32 h-auto'/>
-                <Link to="/">Home</Link> {/* Use Link for navigation */}
+                <Link to="/">Home</Link> 
                 <Link to="/products">Products</Link>
                 <Link to="/cart">Cart</Link>
                 <Link to="/checkout">Check Out</Link>
@@ -24,14 +24,16 @@ const Header = () => {
             </div>
             <div className='flex flex-col items-end'>
             {isMenuOpen && (
-                    <div className='bg-white '>
-                        <div className='m-2'>Home</div>
-                        <div className='m-2'>Products</div>
-                        <div className='m-2'>Cart</div>
-                        <div className='m-2'>Check Out</div>
+                <div>
+                    <div className='bg-white flex flex-col items-center '>
+                        <div className='m-2'><Link to="/">Home</Link></div>
+                        <div className='m-2'><Link to="/products">Products</Link></div>
+                        <div className='m-2'><Link to="/cart">Cart</Link></div>
+                        <div className='m-2'><Link to="/checkout">Check Out</Link></div>
                         <div className='m-2'>My Account</div>
                         <div className='m-2'>Contact Us</div>
                     </div>
+                </div>
                 )}
             </div>
         
--- src/components/ProductChoice.jsx
+++ src/components/ProductChoice.jsx
@@ -8,26 +8,26 @@ const ProductChoice = ({product,name1,price1,rating1,img1,name2,price2,rating2,i
                 </div>
             </div>
     <div className="flex">
-        <div className="flex flex-col justify-between bg-[#A3A30F77] m-4 h-full">
+        <div className="flex flex-col justify-between bg-[#A3A30F77] m-4 h-full w-full md:w-[80%]">
             <div>
                 <img src={img1} alt=""/>
                 <p>{name1}</p>
                 <RatingStars rating={rating1}/>
                 <p>{price1}</p>
             </div>
-            <div>
-                <button className="bg-black text-white ml-auto">Add to cart</button>
+            <div className="flex justify-end">
+                <button className="bg-black p-2 text-white ml-auto rounded-md">Add to cart</button>
             </div>
         </div>
-        <div className="flex flex-col justify-between bg-[#A3A30F77] m-4 h-full">
+        <div className="flex flex-col justify-between bg-[#A3A30F77] m-4 h-full w-full md:w-[80%]">
             <div>
                 <img src={img2} alt=""/>
                 <p>{name2}</p>
                 <RatingStars rating={rating2}/>
                 <p>{price2}</p>
             </div>
-            <div>
-                <button className="bg-black text-white ml-auto">Add to cart</button>
+            <div className="flex justify-end">
+                <button className="bg-black p-2 text-white ml-auto rounded-md">Add to cart</button>
             </div>
         </div>
     </div>
