4cfdb0eeed74bf759fb140efe68f24e7d05125e1
eliguus <naoltd164@gmail.com>
2024-07-09 20:22:18 +0300

    some basic change

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -7,10 +7,10 @@ const Comments = ({name,place,comments}) => {
             <div className='bg-[#A3A30F77] rounded-lg p-4'>
                 <div className="flex items-center justify-between"> 
                     <div className="flex flex-col"> 
-                        <p className='text-4xl'>{name}</p>
-                        <p className='text-2xl'>{place}</p>
+                        <p className='text-3xl md:text-4xl '>{name}</p>
+                        <p className='text-2xl md:text-2xl'>{place}</p>
                     </div>
-                    <img src={quote} alt='quote'/>
+                    <img src={quote} alt='quote' className='w-[10%] md:w-auto'/>
                 </div>
                 <p className='xl'>{comments}</p>
             </div>
