5cb44dc3acc81c53887eff146d9ecf2167d76c79
eliguus <naoltd164@gmail.com>
2024-07-09 18:39:24 +0300

    checkout page for big devices implemented

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,4 +1,5 @@
 import paymentCards from '../assets/CheckOutPage/PaymentCards.png'
+import { FaStar } from 'react-icons/fa'
 const CheckoutPage = () => {
     return (
         <>
@@ -16,7 +17,7 @@ const CheckoutPage = () => {
             </div>
             <div className="flex flex-col items-center my-8">
                 <div className="flex justify-between w-[80%] shadow-[#A3A30F77] shadow-md ">
-                    <p>Returning customer?Click here to login</p>
+                    <p className='py-4 px-2'>Returning customer?<span className='text-[#A3A30F77]'>Click here to login</span></p>
                 </div>
             </div>
             <div className="flex flex-col items-center my-8">
@@ -26,59 +27,59 @@ const CheckoutPage = () => {
                 <form className="space-y-4">
                     <div className="flex justify-between gap-4">
                         <div className="flex-1">
-                        <label htmlFor="firstName" className="block">First name</label>
-                        <input type="text" id="firstName" name="firstName" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
+                            <label htmlFor="firstName" className="inline-flex items-center">First name <FaStar color='red' /></label>
+                            <input type="text" id="firstName" name="firstName" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
                         </div>
                         <div className="flex-1">
-                        <label htmlFor="lastName" className="block">Last name</label>
-                        <input type="text" id="lastName" name="lastName" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
+                            <label htmlFor="lastName" className="inline-flex items-center">Last name <FaStar color='red' /></label>
+                            <input type="text" id="lastName" name="lastName" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
                         </div>
                     </div>
                     <div>
-                        <label htmlFor="countryRegion" className="block">Country/region</label>
+                        <label htmlFor="countryRegion" className="inline-flex items-center">Country/region<FaStar color='red' /></label>
                         <select id="countryRegion" name="countryRegion" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent">
                         {/* Add <option> tags for each country/region */}
                         </select>
                     </div>
                     <div>
-                        <label htmlFor="streetAddress" className="block">Street address</label>
+                        <label htmlFor="streetAddress" className="inline-flex items-center">Street address<FaStar color='red' /></label>
                         <input type="text" id="streetAddress" name="streetAddress" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
                     </div>
                     <div>
-                        <label htmlFor="townCity" className="block">Town/city</label>
+                        <label htmlFor="townCity" className="inline-flex items-center">Town/city<FaStar color='red' /></label>
                         <input type="text" id="townCity" name="townCity" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
                     </div>
                     <div>
-                        <label htmlFor="state" className="block">State</label>
+                        <label htmlFor="state" className="inline-flex items-center">State<FaStar color='red' /></label>
                         <input type="text" id="state" name="state" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
                     </div>
                     <div>
-                        <label htmlFor="postcode" className="block">Postcode (optional)</label>
+                        <label htmlFor="postcode" className="inline-flex items-center">Postcode (optional)<FaStar color='red' /></label>
                         <input type="text" id="postcode" name="postcode" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" />
                     </div>
                     <div>
-                        <label htmlFor="phone" className="block">Phone</label>
+                        <label htmlFor="phone" className="inline-flex items-center">Phone<FaStar color='red' /></label>
                         <input type="tel" id="phone" name="phone" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
                     </div>
                     <div>
-                        <label htmlFor="emailAddress" className="block">Email address</label>
+                        <label htmlFor="emailAddress" className="inline-flex items-center">Email address<FaStar color='red' /></label>
                         <input type="email" id="emailAddress" name="emailAddress" className="mt-1 block w-full border border-[#A3A30F77] bg-transparent" required />
                     </div>
                     </form>
                     </div>
                     <div className="border border-[#A3A30F77] w-[28%]">
-                        <p>Your Order</p>
+                        <p className='text-xl mx-4'>Your Order</p>
                         <div className="flex flex-row">
-                            <div>
-                                <p>Product</p>
-                                <p>Black body hug gown</p>
-                                <p>Female Denim Trousers</p>
+                            <div className='m-2'>
+                                <p className='text-xl my-2'>Product</p>
+                                <p>Black body hug gown x2</p>
+                                <p>Female Denim Trousers x1</p>
                                 <p>Subtotal</p>
                                 <p>Shipping</p>
                                 <p>Total</p>
                             </div>
-                            <div>
-                                <p>Subtotal</p>
+                            <div className='m-2'>
+                                <p className='text-xl my-2'>Subtotal</p>
                                 <p>N 20,000</p>
                                 <p>N 20,000</p>
                                 <p>N 40,000</p>
@@ -87,11 +88,11 @@ const CheckoutPage = () => {
 
                             </div>
                         </div>
-                        <p>Debit/Credit Cards</p>
-                        <div>
+                        <p className='mt-6'>Debit/Credit Cards</p>
+                        <div className='flex items-center flex-col m-4'>
                             <img src={paymentCards} alt=""/>
-                            <p>make payment with your debit card</p>
-                            <button>Place Order</button>
+                            <p className='text-[#A3A30F77]'>make payment with your debit card</p>
+                            <button className='bg-[#A3A30F77] m-6 px-4 py-2'>Place Order</button>
                         </div>
                     </div>
                 </div>
